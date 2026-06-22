aabb15f6c026a8607c4e76daa8c54fb409e2d1b8
Aamina Siraj <aaminasiraj23@gmail.com>
2026-06-23 00:57:17 +0530

    Bsic float and int calculator

---
+++ Week-0/calculator.py
@@ -0,0 +1,13 @@
+# declare variables.
+x = float(input("what's x "))
+y = float(input("whats's y "))
+
+#rounding off 
+
+z = x/y
+ 
+#print result
+print(f"{z:.2f}")
+
+
+#you can put int at a variety of places at your own 
