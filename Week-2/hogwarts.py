6bed6371e6cce526f595e14261f95e48fffb0671
Aamina Siraj <aaminasiraj23@gmail.com>
2026-07-01 11:41:12 +0530

    learnt more aspects of loops

--- Week-2/hogwarts.py
+++ Week-2/hogwarts.py
@@ -1,9 +1,10 @@
 students = [
-    {"name": "Hermione", "house": "Griffindor", "patronus": "Otter"}
-    {"name": "Harry", "house": "Griffindor", "patronus": "Stag"}
-    {"name": "Ron", "house": "Griffindor", "patronus": "Jack russel terrier"}
+    {"name": "Hermione", "house": "Griffindor", "patronus": "Otter"},
+    {"name": "Harry", "house": "Griffindor", "patronus": "Stag"},
+    {"name": "Ron", "house": "Griffindor", "patronus": "Jack russel terrier"},
     {"name": "Draco", "house": "Slytherin", "patronus": None}
 ]
 
 for student in students:
-    print(student["name"])
+
+    print(student["name"], student["house"], student["patronus"], sep = ", ")
